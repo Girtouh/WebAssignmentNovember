a3cc78001ae2f9b3ed5bb581a756faf762ddb8a3
Linus Skagerberg <erako97@gmail.com>
2016-11-22 13:14:29 +0100

    WebUpp 221116 Initial commit. Added a controller, index view and a Contact class.

---
+++ WebUpp 221116/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebUpp_221116.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+
+        public string Message { get; set; }
+
+        public string Mail { get; set; }
+    }
+}
