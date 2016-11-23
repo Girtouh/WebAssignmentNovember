e425a82337b5f0d0980d4c340a3ee07337a4edfe
Linus Skagerberg <erako97@gmail.com>
2016-11-23 15:18:07 +0100

    Added ALOT of things, copied code from w3 schools to learn how to understand some JavaScript elements.

--- WebUpp 221116/Controllers/HomeController.cs
+++ WebUpp 221116/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebUpp_221116.Models;
 
 namespace WebUpp_221116.Controllers
 {
@@ -10,6 +11,11 @@ namespace WebUpp_221116.Controllers
     {
         // GET: Home
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Testing()
         {
             return View();
         }
